adfb6140f3c8da561f4e0d7b12344a150c05bdf8
kevin <proyectosmerkahorros55@gmail.com>
2024-07-16 14:07:04 -0500

    corrijiendo errores

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 app.use(cors());
 app.use(bodyParser.json());
